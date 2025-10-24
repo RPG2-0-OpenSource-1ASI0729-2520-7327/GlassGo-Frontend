e90dcaf507773696501d06ef5cabe0858c176000
Jarod Cespedes <jarod.cespedes@gmail.com>
2025-10-24 16:53:42 -0500

    chore: initial commit featuring i18n setup.

---
+++ src/app/app.ts
@@ -0,0 +1,19 @@
+import {Component, inject, signal} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('GlassGo-Frontend');
+  private translate: TranslateService;
+
+  constructor() {
+    this.translate = inject(TranslateService);
+    this.translate.addLangs(['en', 'es']);
+    this.translate.use('en');
+  }
+}
