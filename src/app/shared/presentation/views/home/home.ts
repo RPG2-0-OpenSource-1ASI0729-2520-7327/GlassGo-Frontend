ad9f8fea343c4af6ee48f40a03a89540a1fe7eae
Guillermo Howard <U202222275@upc.edu.pe>
2025-11-03 21:06:07 -0500

    feat: enhance home component with user stats, tracking, and inventory display.

--- src/app/shared/presentation/views/home/home.ts
+++ src/app/shared/presentation/views/home/home.ts
@@ -1,14 +1,30 @@
-import {Component} from '@angular/core';
-import {TranslatePipe} from '@ngx-translate/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
-  imports: [
-    TranslatePipe
-  ],
+  standalone: true,
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class HomeComponent implements OnInit {
+  userName = 'Usuario';
+  stats: any = {};
+  recentTracking: any[] = [];
+  inventory: any[] = [];
+  calendar: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any>('assets/db.json').subscribe(data => {
+      this.stats = data.stats;
+      this.recentTracking = data.recentTracking;
+      this.inventory = data.inventory;
+      this.calendar = data.calendar;
+      this.userName = data.userName;
+    });
+  }
 }
