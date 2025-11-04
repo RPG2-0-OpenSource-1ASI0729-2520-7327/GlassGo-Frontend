6b5965e94ee168a198b99b9e9c3b34f4940e70ab
Guillermo Howard <U202222275@upc.edu.pe>
2025-11-03 21:05:31 -0500

    feat: update application structure and configuration for improved clarity.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,7 @@
-import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection} from '@angular/core';
-import {provideRouter} from '@angular/router';
-
-import {routes} from './app.routes';
-import {provideHttpClient, withFetch} from '@angular/common/http';
-import {provideTranslateService} from '@ngx-translate/core';
-import {provideTranslateHttpLoader} from '@ngx-translate/http-loader';
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({eventCoalescing: true}),
-    provideRouter(routes),
-    provideHttpClient(withFetch()),
-    provideTranslateService({
-      loader: provideTranslateHttpLoader({prefix: './i18n/', suffix: '.json'}),
-      fallbackLang: 'en'
-    })
-  ]
+  providers: [provideRouter(routes)],
 };
