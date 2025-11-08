525a18c9b9575c1353a33505fefac2c66904f68b
DarkBeider20 <stevexd2016@gmail.com>
2025-11-08 03:03:52 -0500

    feat: added system administration bounded context

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LayoutComponent } from './shared/presentation/components/layout/layout'
 import { HomeComponent } from './shared/presentation/views/home/home';
 import { ComingSoonComponent } from './shared/presentation/views/coming-soon/coming-soon';
 import { PageNotFoundComponent } from './shared/presentation/views/page-not-found/page-not-found';
+import { AdministrationComponent } from './shared/presentation/views/administration/administration';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'app/home', pathMatch: 'full' },
@@ -11,13 +12,16 @@ export const routes: Routes = [
     path: 'app',
     component: LayoutComponent,
     children: [
+       // 🔹 Redirección interna si solo entras a /app
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+
       { path: 'home', component: HomeComponent, title: 'Home - GlassGo' },
       { path: 'crear-pedido', component: ComingSoonComponent, title: 'Crear Pedido - GlassGo' },
       { path: 'tracking', component: ComingSoonComponent, title: 'Tracking - GlassGo' },
       { path: 'inventario', component: ComingSoonComponent, title: 'Inventario - GlassGo' },
       { path: 'reportes', component: ComingSoonComponent, title: 'Reportes - GlassGo' },
       { path: 'calendar', component: ComingSoonComponent, title: 'Calendario - GlassGo' },
-      { path: '', redirectTo: 'home', pathMatch: 'full' }
+      { path: 'administracion', component: AdministrationComponent, title: 'Administracion' }
     ]
   },
 
---
+++ src/app/shared/presentation/views/administration/administration.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-administration',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './administration.html',
+  styleUrls: ['./administration.css']
+})
+export class AdministrationComponent {}
