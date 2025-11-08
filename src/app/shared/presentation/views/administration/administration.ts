ef22bc8c7499d813b27b24784bf33f291701fb85
DarkBeider20 <stevexd2016@gmail.com>
2025-11-08 03:28:56 -0500

    feat: added system administration UI and fakeAPI fetched

--- src/app/shared/presentation/views/administration/administration.ts
+++ src/app/shared/presentation/views/administration/administration.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-administration',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './administration.html',
   styleUrls: ['./administration.css']
 })
-export class AdministrationComponent {}
+export class AdministrationComponent implements OnInit {
+  distribuidoras: any[] = [];
+  licorerias: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('http://localhost:3000/clientes').subscribe({
+      next: (data) => {
+        this.distribuidoras = data.filter(c => c.tipo === 'distribuidora');
+        this.licorerias = data.filter(c => c.tipo === 'licoreria');
+      },
+      error: (err) => console.error('Error al cargar clientes:', err)
+    });
+  }
+}
